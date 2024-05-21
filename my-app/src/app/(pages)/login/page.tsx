ff5bbed922750ee76122cb03182bd7e640382a4e
Jeevaa-Dhanasekaran-ZS0327 <jeevaa.d@zucisystems.com>
2024-05-21 11:53:25 +0530

    1t commit

---
+++ my-app/src/app/(pages)/login/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Login from "@/app/components/login/Login";
+
+const LoginPage = () => {
+  return (
+    <>
+      <Login />
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ my-app/src/app/components/login/Login.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+function Login() {
+  return <div>Hello Nravl!!!</div>;
+}
+
+export default Login;
